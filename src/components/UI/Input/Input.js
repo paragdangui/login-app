b366dbc8fe33c08ebfe7dd671a792eb1ee4ebe5a
paragdangui <paragdangui@gmail.com>
2021-07-21 14:19:19 +0530

    input fields turned into a seperate component

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -1,11 +1,23 @@
-import React from 'react';
-
-const Input = () => {
+import React from "react";
+import classes from "./Input.module.css";
+const Input = (props) => {
 	return (
 		<React.Fragment>
-			asd
-		</React.Fragment>
+			<div
+				className={`${classes.control} ${props.isValid === false ? classes.invalid : ""
+					}`}
+			>
+				<label htmlFor={props.id}>{props.label}</label>
+				<input
+					id={props.id}
+					type={props.type}
+					value={props.value}
+					onChange={props.onChange}
+					onBlur={props.onBlur}
+				/>
+			</div>
+		</React.Fragment >
 	);
-}
+};
 
 export default Input;
